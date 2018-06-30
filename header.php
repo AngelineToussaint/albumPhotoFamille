da41f38640d592080149f69b73dffe6f90b7bf4a
Angéline Toussaint <contact@angeline-toussaint.fr>
2018-06-30 17:31:03 +0200

    Création du footer

---
+++ header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Planning Révision</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+</head>
