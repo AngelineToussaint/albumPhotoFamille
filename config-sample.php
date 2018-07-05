c245a4790ea35152ccd968c7e9e1edf96b8f0785
Adrien <officielghostfire@gmail.com>
2018-07-05 20:28:48 +0200

    Fix

---
+++ config-sample.php
@@ -0,0 +1,6 @@
+<?php
+
+define('db', 'YOUR_DB_HERE');
+define('host', 'YOUR_HOST_HERE');
+define('user', 'YOUR_USER_HERE');
+define('pw', 'YOUR_PASSWORD_HERE');
--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Planning Révision</title>
+    <title>Album photo familial</title>
     <link rel="stylesheet" href="css/styles.css">
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 </head>
