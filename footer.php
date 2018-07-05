35b003caaaee97d78a3dbf694b1af1a0523d77f3
Angéline Toussaint <contact@angeline-toussaint.fr>
2018-07-05 21:10:22 +0200

    Fix HTML structure

--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
-        </div>
-    </div>
     <footer class="footer">
         <h6> Copyright Angéline TOUSSAINT</h6>
     </footer>
--- header.php
+++ header.php
@@ -6,3 +6,4 @@
     <link rel="stylesheet" href="css/styles.css">
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 </head>
+<body>
