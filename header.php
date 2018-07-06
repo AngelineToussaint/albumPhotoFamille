3e792f562a01018178fad96f8f8c8ad1bc17b030
Angéline Toussaint <contact@angeline-toussaint.fr>
2018-07-06 16:49:07 +0200

    Create Menu

--- header.php
+++ header.php
@@ -7,3 +7,35 @@
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 </head>
 <body>
+
+<div class="menu">
+    <div class="title">
+        Album photo
+    </div>
+    <div class="content">
+        <ul>
+            <li>
+                <a href="">Accueil</a>
+            </li>
+            <li>
+                <a href="">Connexion</a>
+            </li>
+            <li>
+                <a href="">Inscription</a>
+            </li>
+            <li>
+                <a href="">Mon album</a>
+            </li>
+            <li>
+                <a href="">Albums partagés</a>
+            </li>
+            <li>
+                <a href="">Notifications</a>
+            </li>
+            <li>
+                <a href="">Déconnexion</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="clear">
+</div>
