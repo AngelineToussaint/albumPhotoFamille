677cc25dd61db9bc01c8a31d6a0f22969611e8f0
Angéline Toussaint <contact@angeline-toussaint.fr>
2018-07-06 16:54:48 +0200

    Show/Hide the menu if the user is logged

--- header.php
+++ header.php
@@ -17,24 +17,34 @@
             <li>
                 <a href="">Accueil</a>
             </li>
-            <li>
-                <a href="">Connexion</a>
-            </li>
-            <li>
-                <a href="">Inscription</a>
-            </li>
-            <li>
-                <a href="">Mon album</a>
-            </li>
-            <li>
-                <a href="">Albums partagés</a>
-            </li>
-            <li>
-                <a href="">Notifications</a>
-            </li>
-            <li>
-                <a href="">Déconnexion</a>
-            </li>
+            <?php
+            if(!isset($_SESSION["utilisateur"])){
+                ?>
+                <li>
+                    <a href="">Connexion</a>
+                </li>
+                <li>
+                    <a href="">Inscription</a>
+                </li>
+                <?php
+            }
+            else{
+                ?>
+                <li>
+                    <a href="">Mon album</a>
+                </li>
+                <li>
+                    <a href="">Albums partagés</a>
+                </li>
+                <li>
+                    <a href="">Notifications</a>
+                </li>
+                <li>
+                    <a href="">Déconnexion</a>
+                </li>
+                <?php
+            }
+            ?>
         </ul>
     </div>
     <hr class="clear">
